8473f3d9fda9897495269a1fafe0715c6f3a68b0
myle2305 <S4028203@student.rmit.edu.au>
2025-04-01 12:29:16 +1100

    Create script.js

---
+++ week 5/cart/script.js
@@ -0,0 +1,7 @@
+let shoppingCart = [
+  { name: "T-shirt", price: 20 },
+  { name: "Jeans", price: 50 },
+  { name: "Sneakers", price: 80 },
+  { name: "Backpack", price: 30 },
+];
+for (let i = 0; i <shoppingCart)
