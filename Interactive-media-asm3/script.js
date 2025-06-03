d093b98240e106edf3abb8f2bf712f4e8e7c187c
myle2305 <S4028203@student.rmit.edu.au>
2025-06-03 23:41:50 +1000

    add drag drop ghost img

--- Interactive-media-asm3/script.js
+++ Interactive-media-asm3/script.js
@@ -23,3 +23,43 @@ const land = new Konva.Image({
 });
 land.scale({ x: 10, y: 10 });
 layer.add(land);
+
+const riceIcon = document.getElementById("rice-icon");
+
+riceIcon.addEventListener("dragstart", function (event) {
+  const ghost = document.getElementById("rice-ghost");
+  ghost.style.display = "";
+
+  event.dataTransfer.setDragImage(ghost, 50, 50);
+  setTimeout(() => {
+    ghost.style.display = "none";
+  }, 0);
+});
+
+const potatoIcon = document.getElementById("potato-icon");
+
+potatoIcon.addEventListener("dragstart", function (event) {
+  const ghost = document.getElementById("potato-ghost");
+  ghost.style.display = "";
+
+  event.dataTransfer.setDragImage(ghost, 50, 50);
+  setTimeout(() => {
+    ghost.style.display = "none";
+  }, 0);
+});
+
+const cornIcon = document.getElementById("corn-icon");
+
+cornIcon.addEventListener("dragstart", function (event) {
+  const ghost = document.getElementById("corn-ghost");
+  ghost.style.display = "";
+
+  event.dataTransfer.setDragImage(ghost, 50, 50);
+  setTimeout(() => {
+    ghost.style.display = "none";
+  }, 0);
+});
+
+cornIcon.addEventListener("dragend", function (event) {
+  console.log(event);
+});
