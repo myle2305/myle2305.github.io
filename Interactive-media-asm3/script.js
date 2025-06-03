672199c2322c1229041bd49dd8c84c614ebbe0d9
myle2305 <S4028203@student.rmit.edu.au>
2025-06-04 00:49:11 +1000

    drag drop harvest

--- Interactive-media-asm3/script.js
+++ Interactive-media-asm3/script.js
@@ -1,9 +1,10 @@
+const konvaSize = 800;
+const landSize = 400;
+
 const stage = new Konva.Stage({
   container: "game-container",
-  width: 800,
-  height: 800,
-  x: 100,
-  y: 100,
+  width: konvaSize,
+  height: konvaSize,
 });
 
 const layer = new Konva.Layer();
@@ -14,16 +15,14 @@ landImg.src = "assets/land.png";
 
 const land = new Konva.Image({
   image: landImg,
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  width: 100,
-  height: 100,
-  offsetX: 50,
-  offsetY: 70,
+  x: stage.width() / 2 - landSize / 2,
+  y: stage.height() / 2 - landSize / 2,
+  width: landSize,
+  height: landSize,
 });
-land.scale({ x: 10, y: 10 });
 layer.add(land);
-
+let layerCrop = null;
+let cropType = null;
 const riceIcon = document.getElementById("rice-icon");
 
 riceIcon.addEventListener("dragstart", function (event) {
@@ -60,6 +59,74 @@ cornIcon.addEventListener("dragstart", function (event) {
   }, 0);
 });
 
-cornIcon.addEventListener("dragend", function (event) {
-  console.log(event);
+riceIcon.addEventListener("dragend", function (event) {
+  const konvaContent = document.getElementsByClassName("konvajs-content")[0];
+  let startX =
+    konvaContent.getBoundingClientRect().left +
+    stage.width() / 2 -
+    landSize / 2;
+  let startY =
+    konvaContent.getBoundingClientRect().top +
+    stage.height() / 2 -
+    landSize / 2;
+  let endX = startX + landSize;
+  let endY = startY + landSize;
+  changeCropLayer("assets/rice1.png");
+  if (
+    event.x >= startX &&
+    event.x <= endX &&
+    event.y >= startY &&
+    event.y <= endY
+  ) {
+    setTimeout(() => {
+      changeCropLayer("assets/rice2.png");
+      setTimeout(() => {
+        changeCropLayer("assets/rice3.png");
+        setTimeout(() => {
+          changeCropLayer("assets/rice4.png");
+        }, 3000);
+      }, 3000);
+    }, 3000);
+    cropType = "rice";
+  }
+});
+
+function changeCropLayer(name) {
+  if (layerCrop != null) {
+    layerCrop.destroy();
+  }
+  const cropImg = new Image();
+  cropImg.src = name;
+  layerCrop = new Konva.Image({
+    image: cropImg,
+    x: stage.width() / 2 - landSize / 2,
+    y: stage.height() / 2 - landSize / 2,
+    width: landSize,
+    height: landSize,
+  });
+  layer.add(layerCrop);
+}
+
+document.getElementById("knife-button").addEventListener("dragend", (event) => {
+  const konvaContent = document.getElementsByClassName("konvajs-content")[0];
+  let startX =
+    konvaContent.getBoundingClientRect().left +
+    stage.width() / 2 -
+    landSize / 2;
+  let startY =
+    konvaContent.getBoundingClientRect().top +
+    stage.height() / 2 -
+    landSize / 2;
+  let endX = startX + landSize;
+  let endY = startY + landSize;
+  if (
+    event.x >= startX &&
+    event.x <= endX &&
+    event.y >= startY &&
+    event.y <= endY
+  ) {
+    if (cropType == "rice") {
+      changeCropLayer("assets/rice5.png");
+    }
+  }
 });
